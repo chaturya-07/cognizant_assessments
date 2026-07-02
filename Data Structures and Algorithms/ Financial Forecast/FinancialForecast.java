323c8b4fbf883c83e4d3b8339d4e3754fbfd4c60
chaturya-07 <vankachaturya@gmail.com>
2026-07-02 17:07:58 +0530

    Create  FinancialForecast.java

---
+++ Data Structures and Algorithms/ Financial Forecast/FinancialForecast.java
@@ -0,0 +1,25 @@
+public class FinancialForecast {
+
+    
+    public static double futureValue(double value, double rate, int years) {
+
+        
+        if (years == 0) {
+            return value;
+        }
+
+        // Recursive case
+        return futureValue(value * (1 + rate), rate, years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double initialValue = 1000; 
+        double growthRate = 0.10;  
+        int years = 5;
+
+        double result = futureValue(initialValue, growthRate, years);
+
+        System.out.println("Future Value after " + years + " years: " + result);
+    }
+}
